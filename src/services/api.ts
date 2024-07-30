74f7fa054f448e911fd28b3c5f516ccae3e58896
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-07-30 08:10:26 +0530

    asda

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `https://employee-management-api-alpha.vercel.app/api/v1/`,
+    baseURL: `https://management-api-alpha.vercel.app/api/v1/`,
     timeout: 10000,
     withCredentials: true
 });
