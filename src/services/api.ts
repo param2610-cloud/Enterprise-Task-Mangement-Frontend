4abd21db330bc3aea57b2ffe3cd63478d3eed16e
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-07-30 08:04:05 +0530

    cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1/',
+    baseURL: `${process.env.VITE_API_URL}/api/v1/`,
     timeout: 10000,
     withCredentials: true
 });
